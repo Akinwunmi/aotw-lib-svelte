744387c0704968fbdec4798f7853895fe27d9418
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2022-06-24 20:10:46 +0200

    create library

---
+++ src/lib/Input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let label;
+</script>
+
+<svelte:options tag="aotw-input" />
+
+{#if label}
+<label for="input">{label}</label>
+{/if}
+<input type="text" id="input" />
+
+<style lang="scss" src="./Input.scss"></style>
